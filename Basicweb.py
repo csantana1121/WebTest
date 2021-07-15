b162e836c636d9032e7cb51d6833464542fb2d2a
Christian Santana <christians1121@gmail.com>
2021-07-15 16:15:54 +0000

    Added Register page redirection for non-unique usernames/emails

--- Basicweb.py
+++ Basicweb.py
@@ -23,11 +23,10 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-  # password = db.Column(db.String(60), nullable=False)
+
 
     def __repr__(self):
-        # passwordhash = bcrypt.generate_password_hash(self.password)
-        return f"User('{self.username}', '{self.email}', '{self.password}')" # # , '{passwordhash}'
+        return f"User('{self.username}', '{self.email}', '{self.password}')" 
 
 @app.route("/")
 @app.route("/home")
@@ -47,11 +46,21 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
         passwordhash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=passwordhash)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+        username = db.session.query(User.id).filter_by(username=form.username.data).first() is not None
+        if username is False:
+            mail = db.session.query(User.id).filter_by(email=form.email.data).first() is not None
+            if mail is False:
+                user = User(username=form.username.data, email=form.email.data, password=passwordhash)
+                db.session.add(user)
+                db.session.commit()
+                flash(f'Account created for {form.username.data}!', 'success')
+                return redirect(url_for('home')) # if so - send to home page
+            else:
+                flash(f'That email is already taken please try another','danger')
+                return redirect(url_for('register'))
+        else:
+            flash(f'That username is already taken please try another','danger')
+            return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -60,7 +69,6 @@ def login():
     if form.validate_on_submit():
         username = db.session.query(User.id).filter_by(username=form.username.data).first() is not None
         if username is True:
-            # passwordhash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
             password = db.session.query(User.password).filter_by(username=form.username.data).first()
             password = password[0]
             if bcrypt.check_password_hash(password, form.password.data):
